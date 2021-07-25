a2c3bd2ce049114f99b76d358323a36d615ac36f
Patrik Galina <pgalina@foi.hr>
2021-07-25 21:47:49 +0200

    Implemented Redux Reducer for Cart

---
+++ src/actions/cartActions.js
@@ -0,0 +1,42 @@
+import {
+  CART_ADD_ITEM,
+  CART_REMOVE_ITEM,
+  CART_SAVE_SHIPPING_ADDRESS,
+  CART_SAVE_PAYMENT_METHOD,
+} from "../constants/cartConstants";
+import api from "../utils/api";
+
+export const addToCart =
+  (productId, quantity) => async (dispatch, getState) => {
+    const { data } = await api.get(`/products/${productId}`);
+    dispatch({
+      type: CART_ADD_ITEM,
+      payload: {
+        naziv: data.naziv,
+        brand: data.brand,
+        cijena: data.cijena,
+        dostupnaKolicina: data.dostupnaKolicina,
+        id: data.id,
+        kolicina: quantity,
+      },
+    });
+    localStorage.setItem(
+      "cartItems",
+      JSON.stringify(getState().cart.cartItems)
+    );
+  };
+
+export const removeFromCart = (productId) => async (dispatch, getState) => {
+  dispatch({ type: CART_REMOVE_ITEM, payload: productId });
+
+  localStorage.setItem("cartItems", JSON.stringify(getState().cart.cartItems));
+};
+
+export const saveShippingAddress = (data) => async (dispatch) => {
+  dispatch({ type: CART_SAVE_SHIPPING_ADDRESS, payload: data });
+  localStorage.setItem("shippingAddress", JSON.stringify(data));
+};
+
+export const savePaymentMethod = (data) => async (dispatch) => {
+  dispatch({ type: CART_SAVE_PAYMENT_METHOD, payload: data });
+};
