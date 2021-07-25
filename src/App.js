b1e5e07cdb6d60126efb0bec3380b1a3bcf672df
Patrik Galina <pgalina@foi.hr>
2021-07-25 21:31:57 +0200

    Created project structure and overall layout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
 import React from "react";
+import { BrowserRouter, Link, Route } from "react-router-dom";
+import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
 
 function App() {
-  return <div></div>;
+  return (
+    <BrowserRouter>
+      <div className="body__gridContainer">
+        <header className="row">
+          <div>
+            <Link className="header__logo" to="/">
+              shoppinger
+            </Link>
+          </div>
+          <div>
+            <Link to="/cart">
+              <ShoppingCartIcon
+                style={{ height: "23px", width: "23px", marginRight: "15px" }}
+              />
+            </Link>
+          </div>
+        </header>
+        <main></main>
+        <footer className="row center">&copy; 2021. All right reserved</footer>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
