9999092042a908d0fbf9ff2b02c3193d24fed388
Patrik Galina <pgalina@foi.hr>
2021-07-26 10:04:00 +0200

    Added condition rendering [check if Cart contains Product]

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,8 +1,9 @@
 import React from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import Rating from "./Rating";
 import { makeStyles } from "@material-ui/core/styles";
 import AddShoppingCartIcon from "@material-ui/icons/AddShoppingCart";
+import RemoveShoppingCartIcon from "@material-ui/icons/RemoveShoppingCart";
 import Button from "@material-ui/core/Button";
 
 import { addToCart, removeFromCart } from "../actions/cartActions";
@@ -17,12 +18,22 @@ const Product = ({ product }) => {
   const dispatch = useDispatch();
   const classes = useStyles();
 
-  console.log("PRODUCT: ", product);
+  const cart = useSelector((state) => state.cart);
+  const isProductInCart = cart.filter((x) => x.id === product.id).length > 0;
 
-  const handleAddToCartClick = (event) => {
+  console.log("IN CART? ", isProductInCart);
+
+  /*console.log("PRODUCT: ", product);
+  console.log("CART: ", cart);*/
+
+  const handleAddToCartClick = () => {
     console.log("ADD TO CART PRODUCT ", product.id);
 
     dispatch(addToCart(product.id));
+  };
+
+  const handleRemoveFromCart = () => {
+    console.log("REMOVE PRODUCT FROM CART ", product.id);
   };
 
   return (
@@ -37,16 +48,28 @@ const Product = ({ product }) => {
         <Rating rating={4.5} numberOfReviews={10} />
         <div className="card__body__footer">
           <div className="card__body__price">{product.cijena} HRK</div>
-          <Button
-            variant="contained"
-            color="primary"
-            disabled={product.dostupnaKolicina < 1}
-            className={classes.button}
-            endIcon={<AddShoppingCartIcon />}
-            onClick={handleAddToCartClick}
-          >
-            {product.dostupnaKolicina < 1 ? "Out of Stock" : "Add to Cart"}
-          </Button>
+          {isProductInCart ? (
+            <Button
+              variant="contained"
+              color="secondary"
+              className={classes.button}
+              endIcon={<RemoveShoppingCartIcon />}
+              onClick={handleRemoveFromCart}
+            >
+              Remove from Cart
+            </Button>
+          ) : (
+            <Button
+              variant="contained"
+              color="primary"
+              disabled={product.dostupnaKolicina < 1}
+              className={classes.button}
+              endIcon={<AddShoppingCartIcon />}
+              onClick={handleAddToCartClick}
+            >
+              {product.dostupnaKolicina < 1 ? "Out of Stock" : "Add to Cart"}
+            </Button>
+          )}
         </div>
       </div>
     </div>
